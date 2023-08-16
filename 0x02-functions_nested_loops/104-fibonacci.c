26174a8b49c2c84ee4f965395520cca5c9eef8a0
HamzaBachir <hamzabachir2001@gmail.com>
2023-08-15 19:00:35 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Prints the first 98 Fibonacci numbers, starting with 
  * 1 and 2, seperated by a comma followed by a space.
  * Return: Always 0.
  */
 int main(void)
 {
 int count;
-unsigned long fib 1 = 0, fib2 =1, sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 for (count = 0; count < 92; count++)
